6f60f88ba0af952fd7065dcc261f9d7f2aa245ea
Himanshu <himanshusinghbisht71@gmail.com>
2026-08-12 00:10:35 +0530

    how to create tokens

---
+++ middleware/authmiddleware.js
@@ -0,0 +1,6 @@
+import jwt from "jsonwebtoken";
+
+function authMiddleware(req, res, next){
+    
+
+}
--- routes/auth.js
+++ routes/auth.js
@@ -16,5 +16,30 @@ router.post("/login", async (req, res)=>{
         });
     }
     // 2. compare password
-    const isPasswordCorrect = await bcrypt.compare9
-})
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+    if(!isPasswordCorrect){
+        return res.status(401).json({
+            message: "Invalid email or password"
+        });
+    }
+    // 3. create jwt token
+    const token = jwt.sign({
+        userid : user.id,
+        email : user.email,
+
+    },
+    process.env.JWT_SECRET_KEY,
+    {
+        expiresIn: "1h",
+    }
+);
+    // 4. send token
+    res.json(
+        {
+            message: "successful login",
+            token,
+        }
+    );
+});
+module.exports = router;
