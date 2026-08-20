b5d8fd541f42603433400e0f6b44e97c355fee0d
Himanshu <himanshusinghbisht71@gmail.com>
2026-08-20 23:28:44 +0530

    keep on working kiddo u got this

--- middleware/rolemiddleware.js
+++ middleware/rolemiddleware.js
@@ -7,3 +7,31 @@ const roleaccess = (role)=>{
     const role  = req.
 
 }
+
+
+
+
+app.get("/", (req,res)=> {
+  res.send("Hello World");
+});
+
+
+
+app.get("/about", (req,res)=>{
+  res.send("About Page");
+
+});
+function logger(req, res, next) {
+  console.log(`${req.method} ${req.url}`);
+  next(); // <- without this, the request hangs forever
+}
+
+app.use(logger);
+
+app.get("/user/:id",(req,res)=>{
+  const id = req.params.id;
+  res.send(`UserID: ${id}`);
+})
+app.listen(3000,()=>{
+  console.log("server running on port: 3000")
+});
